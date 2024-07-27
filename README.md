37405c55ac4cd7335e96bc3a7b6a2788af37380a
Joseph Mudd <137225181+josephdanielm@users.noreply.github.com>
2024-07-27 14:23:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# React Template
+
+Starter project template with:
+
+- React
+- Tailwind CSS
+- ESLint
+- Prettier
+- Vite
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return <>Hello React</>;
+}
+
+export default App;
